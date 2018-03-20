be2d3cc022b7ce77e1e3ad26a5f2c61c0d94fa21
Owen <37493791+hoewen@users.noreply.github.com>
2018-03-19 20:25:12 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,27 +5,32 @@ client.on('message', message => {
      if (message.author.bot) return;
      if(message.content.toLowerCase().includes("made"))
                message.channel.send("That's right, a maid. A maid I can bang.")
-               
+        }
+
 });
 
 client.on('message', message => {
      if (message.author.bot) return;
      if(message.content.toLowerCase().includes("kid"))
                message.channel.send("We gotta definitely write a song about how we do not diddle kids! 'Do not diddle kids, it's no good diddling kids.'")
-               
+       }        
+
 });
 
 client.on('message', message => {
      if (message.author.bot) return;
      if(message.content.toLowerCase().includes("oof"))
                message.channel.send("Ah! Oh! Oh! Botched toe! I botched that one. Oh, that's a botch job. That's bleeding. I need some trash to plug up the cut.")
-               
+       }        
+
 });
 
 client.on('message', message => {
      if (message.author.bot) return;
      if(message.content.toLowerCase().includes("who"))
                message.channel.send("He's the mortician. I invited him.")
+       }
 
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
